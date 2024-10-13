51d3d679f3d7ddd629ec7a8300b4341e94fa6f7b
Binfh <binhkhong2410@gmail.com>
2024-10-13 22:31:49 +0700

    header1

--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,6 +1,9 @@
-function DefaultLayout({ children }) {
+import Header from '../components/Header';
+
+function HeaderOnly({ children }) {
     return (
         <div>
+            <Header />
             <div className="container">
                 <div className="content">{children}</div>
             </div>
@@ -8,4 +11,4 @@ function DefaultLayout({ children }) {
     );
 }
 
-export default DefaultLayout;
+export default HeaderOnly;
